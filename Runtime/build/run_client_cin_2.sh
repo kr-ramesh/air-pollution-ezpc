76488d89fc929987a7256e13ee07a47ab0cd0409
Anwesh Bhattacharya <anwesh.bh@gmail.com>
2022-07-03 20:25:56 +0530

    added cin_2 files

---
+++ Runtime/build/run_client_cin_2.sh
@@ -0,0 +1,9 @@
+./gcnlstm-cin-floatml r=2 nt=16 add=10.0.0.4 > weights12.txt
+
+./gcnlstm-cin-floatml r=2 nt=16 add=10.0.0.4 > weights13.txt
+
+./gcnlstm-cin-floatml r=2 nt=16 add=10.0.0.4 > weights14.txt
+
+./gcnlstm-cin-floatml r=2 nt=16 add=10.0.0.4 > weights15.txt
+
+./gcnlstm-cin-floatml r=2 nt=16 add=10.0.0.4 > weights16.txt
---
+++ Runtime/build/run_server_cin_2.sh
@@ -0,0 +1,15 @@
+printf "FloatML 12 Dec\n"
+cat ../12Dec/inputs2.txt ../12Dec/A.inp ../12Dec/kernel.inp ../12Dec/bias.inp ../12Dec/A2.inp ../12Dec/kernel2.inp ../12Dec/bias2.inp ../12Dec/hidden.txt ../12Dec/cell.txt ../12Dec/kernel3.inp ../12Dec/recurrent_kernel3.inp ../12Dec/bias3.inp ../12Dec/kernel4.inp ../12Dec/bias4.inp ../12Dec/labels2.txt | ./gcnlstm-cin-floatml r=1 nt=16
+
+printf "FloatML 13 Dec\n"
+cat ../13Dec/inputs2.txt ../13Dec/A.inp ../13Dec/kernel.inp ../13Dec/bias.inp ../13Dec/A2.inp ../13Dec/kernel2.inp ../13Dec/bias2.inp ../13Dec/hidden.txt ../13Dec/cell.txt ../13Dec/kernel3.inp ../13Dec/recurrent_kernel3.inp ../13Dec/bias3.inp ../13Dec/kernel4.inp ../13Dec/bias4.inp ../13Dec/labels2.txt | ./gcnlstm-cin-floatml r=1 nt=16
+
+printf "FloatML 14 Dec\n"
+cat ../14Dec/inputs2.txt ../14Dec/A.inp ../14Dec/kernel.inp ../14Dec/bias.inp ../14Dec/A2.inp ../14Dec/kernel2.inp ../14Dec/bias2.inp ../14Dec/hidden.txt ../14Dec/cell.txt ../14Dec/kernel3.inp ../14Dec/recurrent_kernel3.inp ../14Dec/bias3.inp ../14Dec/kernel4.inp ../14Dec/bias4.inp ../14Dec/labels2.txt | ./gcnlstm-cin-floatml r=1 nt=16
+
+printf "FloatML 15 Dec\n"
+cat ../15Dec/inputs2.txt ../15Dec/A.inp ../15Dec/kernel.inp ../15Dec/bias.inp ../15Dec/A2.inp ../15Dec/kernel2.inp ../15Dec/bias2.inp ../15Dec/hidden.txt ../15Dec/cell.txt ../15Dec/kernel3.inp ../15Dec/recurrent_kernel3.inp ../15Dec/bias3.inp ../15Dec/kernel4.inp ../15Dec/bias4.inp ../15Dec/labels2.txt | ./gcnlstm-cin-floatml r=1 nt=16
+
+printf "FloatML 16 Dec\n"
+cat ../16Dec/inputs2.txt ../16Dec/A.inp ../16Dec/kernel.inp ../16Dec/bias.inp ../16Dec/A2.inp ../16Dec/kernel2.inp ../16Dec/bias2.inp ../16Dec/hidden.txt ../16Dec/cell.txt ../16Dec/kernel3.inp ../16Dec/recurrent_kernel3.inp ../16Dec/bias3.inp ../16Dec/kernel4.inp ../16Dec/bias4.inp ../16Dec/labels2.txt | ./gcnlstm-cin-floatml r=1 nt=16
+
